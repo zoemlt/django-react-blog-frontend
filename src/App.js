83d7db2e3e4b3e9124e6d682d5d85e065eece435
zoe <zoejniu@gmail.com>
2022-01-30 18:03:40 +1300

    Fetch data from backend and store in state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import Posts from "./components/Posts";
 
 function App() {
-  return <div className="App"></div>;
+  const [posts, setPosts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const url = "http://127.0.0.1:8000/api";
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setPosts(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Latest Posts</h1>
+      {isLoading ? <p>Loading posts...</p> : <Posts data={posts} />}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Posts.js
@@ -0,0 +1,5 @@
+const Posts = ({ data }) => {
+  return <div>{data[0].title}</div>;
+};
+
+export default Posts;
