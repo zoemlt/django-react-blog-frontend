4109ffb76b5b80aeaa1663b8f74e6bffd4f23004
zoe <zoejniu@gmail.com>
2022-01-30 15:48:33 +1300

    Create header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
-import React from "react";
+import { AppBar, Toolbar, Typography, CssBaseline } from "@mui/material";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="static" elevation={0} color="default">
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Zoe's Blog
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
 };
 
 export default Header;
